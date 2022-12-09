470d25af51eeb68a41e2ae627463d8a3db83b749
qwerewqwerew <qwerew0@gmail.com>
2022-12-09 22:17:27 +0900

    first

---
+++ src/components/MainPage.js
@@ -0,0 +1,61 @@
+import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
+import "./MainPage.css";
+import axios from "axios";
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+dayjs.extend(relativeTime);
+
+const MainPage = () => {
+	let [products, setProducts] = React.useState([]);
+	useEffect(() => {
+		axios
+			.get("http://localhost:8080/products")
+			.then((res) => {
+				products = res.data.product;
+				setProducts(products);
+				console.log(products)
+			})
+			.catch((err) => {
+				return console.log(err);
+			});
+	}, []);
+	if (products === undefined) {
+		return <h1>상품정보를 받고 있습니다.</h1>;
+	}
+	return (
+		<>
+			<div id="body">
+				<div id="banner">
+					<img src="images/banners/banner1.png" alt="" />
+				</div>
+				<h2>Products</h2>
+				<div id="product-list">
+					{products.map((product, idx) => {
+						return (
+							<div className="product-card" key={idx}>
+								<Link className="product-link" to={`/product/${product.id}`}>
+									<div>
+										<img className="product-img" src={product.imageUrl} alt={product.name} />
+									</div>
+									<div className="product-content">
+										<span className="product-name">{product.name}</span>
+										<span className="product-price">{product.price}원</span>
+										<div className="product-footer">
+											<div className="product-seller">
+												<img className="product-avatar" src="images/icons/avatar.png" alt="" />
+												<span>{product.seller}</span>
+											</div>
+											<span className="product-date">{dayjs(product.createdAt).fromNow()}</span>
+										</div>
+									</div>
+								</Link>
+							</div>
+						);
+					})}
+				</div>
+			</div>
+		</>
+	);
+};
+export default MainPage;
---
+++ src/components/UploadPage.js
@@ -0,0 +1,66 @@
+import { useState } from "react";
+import "./UploadPage.css";
+
+import { Form, Divider, Input, InputNumber, Button, Upload } from "antd";
+const { TextArea } = Input;
+
+const UploadPage = () => {
+	const [imageUrl, setImageUrl] = useState(null);
+	const onSubmit = (values) => {
+		console.log("이것은 벨류", values);
+	};
+	const onChageImage = (info) => {
+		if (info.file.status === "uploading") {
+			return;
+		}
+		if (info.file.status === "done") {
+			const response = info.file.response;
+			const imageUrl = response.imageUrl;
+			setImageUrl(imageUrl)
+		}
+	};
+
+	return (
+		<div id="body">
+			<div id="load-container">
+				<Form name="uploadForm" onFinish={onSubmit}>
+					<Form.Item name="upload" label={<div className="upload-label">상품 사진</div>}>
+						<Upload name="image" action="http://localhost:8080/image" listType="picture" showUploadList={false} onChange={onChageImage}>
+							{imageUrl ? (
+								<img id="upload-img" src={`http://localhost:8080/${imageUrl}`}/>
+							):(
+								<div id="upload-img-placeholder">
+									<img src="/images/icons/camera.png" /> 
+									<span>이미지업로드</span>
+								</div>
+							)}
+						</Upload>
+					</Form.Item>
+					<Divider />
+					<Form.Item label={<div className="upload-label">판매자명</div>} name="seller" rules={[{ required: true, message: "판매자명을 입력해주세요" }]}>
+						<Input className="upload-name" size="large" placeholder="판매자명을 입력해주세요" />
+					</Form.Item>
+					<Divider />
+					<Form.Item label={<div className="upload-label">상품명</div>} rules={[{ required: true, message: "상품명을 입력해주세요" }]} name="name">
+						<Input className="upload-name" size="large" placeholder="상품명을 입력해주세요" />
+					</Form.Item>
+					<Divider />
+					<Form.Item label={<span className="upload-label">판매가</span>} name="product-price" rules={[{required: true, message: '판매가는 필수 입력 사항입니다',},]}>
+						<InputNumber />
+				  </Form.Item>
+				  <Divider />
+					<Form.Item label={<div className="upload-label">상품설명</div>} rules={[{ required: true, message: "상품설명을 입력해주세요" }]} name="description">
+						<TextArea size="large" id="product-description" showCount maxLength={300} placeholder="상품설명을 입력해주세요" />
+					</Form.Item>
+					<Divider />
+					<Form.Item>
+						<Button id="submit-button" size="large" htmlType="submit">
+							상품등록하기
+						</Button>
+					</Form.Item>
+				</Form>
+			</div>
+		</div>
+	);
+};
+export default UploadPage;
